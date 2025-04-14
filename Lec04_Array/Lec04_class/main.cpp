522f345b49951927cf78aceab628f04aa693bf38
leewonjun11 <toplaw_4977@naver.com>
2025-04-14 09:55:21 +0900

    Lec04

--- Lec04_Array/Lec04_class/main.cpp
+++ Lec04_Array/Lec04_class/main.cpp
@@ -1,8 +1,25 @@
 //#include "Complex.h"
-#include "Car.h"
-#include "SportsCar.h"
+//#include "Car.h"
+//#include "SportsCar.h"
+#include "Music.h"
 int main()
 {
+	//music streaming service¸¦ »ý¼º
+	MusicStreamingService my_service("spotify");
+	my_service.addMusic("polaroidLove", "ENHYPEN", "DIMENSION", 2022);
+	my_service.addMusic("Ditto", "NewJeans", "ALBUM", 2023); 
+	my_service.addMusic("CAKE", "ITZY", "ALBUM", 2023);
+	my_service.addMusic("sundayMorning", "asdasd", "ALBUM", 2023);
+	// search music by title
+	string music_title;
+	cout << "Enter the Music Title; ";
+	cin >> music_title;
+	Music* result = my_service.searchByTitle(music_title);
+	if (result != NULL) {
+		cout << "Found: << result ->getTitle() << "by" <<result->
+	}
+
+
 	//	Complex a, b, c;
 //	a.read("A=");
 //	a.print("A =");
@@ -20,4 +37,10 @@ int main()
 
 	mySecondCar.setTurbo(true);
 	
+}
+
+Music* result = my_service.searchByTitle(music_title);
+if (result != NULL) {
+	cout<< "Found:" << result->getTitle() << "by" << result.size() << "somgs by" << artist_name << ";" << endl;
+	for(int i = 0)
 }
